4e82801d1617b7a9a88f78c73929b84c696cf07a
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-01 02:37:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,8 +5,8 @@ class Application
 
     if req.path.match /items/
       item_from_path = req.path.split(/items./).last
-      if @@item.include?(item)
-        resp.write @@item.detect {|item| item.name = item_from_path}.price
+      if Item.item.include?(item)
+        resp.write Item.item.detect {|item| item.name = item_from_path}.price
       else
         resp.write "No such item"
       end
