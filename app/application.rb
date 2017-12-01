0cef8f2d3cf411429dacc5b02e0be223dfa82319
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-01 02:23:54 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+  def call
+    resp = Rack::Response.new
+    req = Rack::Request.new
+
+    if req.path.match /item/
+      item = req.path.split(/item/).last
+      resp.write item.price
+    else
+      resp.write "Page not Found"
+      resp.status 404
+      
+    resp finish
+  end
+end
